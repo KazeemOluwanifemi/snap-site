2d898652006ae35b8a6cbeae96c575229af75d0c
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-07-21 00:34:23 +0100

    made font updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'almost-white' : 'hsl(0, 0%, 98%)',
+        'medium-gray': 'hsl(0, 0%, 41%)',
+        'almost-black': 'hsl(0, 0%, 8%)'
+      },
+      fontFamily: {
+        'epilogue' : ['Epilogue', 'sans-serif'],
+        url:('https://fonts.googleapis.com/css2?family=Epilogue:ital,wght@0,100..900;1,100..900&display=swap'),
+      }
+    },
   },
   plugins: [],
 }
