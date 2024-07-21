390c5e7d406b545dbcab9a371357720accf0bd85
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-07-21 19:37:14 +0100

    included button component

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,9 +1,12 @@
+import Button from "./button"
+
 export default function Main(){
-    return <div className="border-solid border-2 border-almost-black m-24 flex flex-row justify-center">
-        <div className="main-text w-1/2">
+    return <div className="border-solid border-2 border-almost-black m-24 flex flex-row">
+        <div className="main-text w-1/2 justify-around">
             <h1 className="font-bold text-almost-black text-9xl">Make remote work</h1>
             <p className="text-lg text-medium-gray">Get your team in sync, no matter your location.
             Streamline processes, create team rituals, and watch productivity soar.</p>
+            <Button></Button>
         </div>
         <img src="/proj-assets/images/image-hero-desktop.png" className="w-1/2"></img>
     </div>
