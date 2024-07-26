a44b8f11d7976bdc9180aa3586a1c839d504cf0e
KazeemOluwanifemi <bethkaz934@gmail.com>
2024-07-26 16:54:22 +0100

    made adjustments to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@
 import Navbar from './components/navbar.tsx';
 // import Button from './components/button.tsx';
 import Main from './components/main.tsx';
-import DropdownFeatures from './components/dropdown-features.tsx';
-import DropdwonCompany from './components/dropdown-company.tsx';
+// import DropdownFeatures from './components/dropdown-features.tsx';
+// import DropdwonCompany from './components/dropdown-company.tsx';
 
 export default function App() {
   return (
     <body className="font-epilogue">
       <Navbar></Navbar>
-      <DropdownFeatures></DropdownFeatures>
-      <DropdwonCompany></DropdwonCompany>
       <Main></Main>
     </body>
   )
